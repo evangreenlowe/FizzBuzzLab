c88360622035401dd6d53572081148958fc027ff
Evan Green-Lowe <evangreenlowe@gmail.com>
2015-04-25 14:38:24 +0100

    Edit js script

---
+++ js/fizz-buzz.js
@@ -0,0 +1,20 @@
+var fizzBuzz = function fizzBuzz(arg){
+	if (arg % 3 ===0 & arg % 5===0){
+		return 'FizzBuzz';
+	}
+	
+	else if(arg % 3 === 0){
+		return 'Fizz';
+	}
+
+	else if(arg % 5 === 0){
+		return 'Buzz';
+	}
+
+	else{
+		return arg;
+	}
+
+}
+
+fizzBuzz(15);
